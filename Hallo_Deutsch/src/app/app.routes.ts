e443fca09ab531b02dfdec68f9c203ad35c24b48
Stephaniiee <stefstefi23@yahoo.com>
2025-03-04 20:02:04 +0200

    added a slider between login and signup components

--- Hallo_Deutsch/src/app/app.routes.ts
+++ Hallo_Deutsch/src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication/authentication.component';
+import { LoginComponent } from './authentication/login/login.component';
+import { SignupComponent } from './authentication/signup/signup.component';
 
 export const routes: Routes = [
   {path: 'authentication', component: AuthenticationComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent},
+  { path: '**', redirectTo: '' }
 ];
--- Hallo_Deutsch/src/app/authentication/authentication.component.ts
+++ Hallo_Deutsch/src/app/authentication/authentication.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @Component({
   selector: 'app-authentication',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent, SignupComponent, CommonModule],
   templateUrl: './authentication.component.html',
   styleUrl: './authentication.component.scss'
 })
 export class AuthenticationComponent {
+  constructor(
+    private router: Router
+  ) {}
 
+  activeTab: string = 'login';
+
+  setTab(tabname: string) {
+    this.activeTab = tabname;
+  }
 }
