0bbb59f11c5628876fb964a08111458bce864341
Stephaniiee <stefstefi23@yahoo.com>
2025-04-26 08:42:19 +0300

    code update

--- Server-Management/src/app/dashboard/dashboard.component.ts
+++ Server-Management/src/app/dashboard/dashboard.component.ts
@@ -3,6 +3,7 @@ import { ServerStatusComponent } from './server-status/server-status.component';
 import { TrafficDataService } from './traffic-data.service';
 import { TrafficMonitorizationComponent } from './traffic-monitorization/traffic-monitorization.component';
 import { SupportTicketsComponent } from './support-tickets/support-tickets.component';
+import { DashboardItemComponent } from './dashboard-item/dashboard-item.component';
 
 @Component({
   selector: 'app-dashboard',
@@ -11,6 +12,7 @@ import { SupportTicketsComponent } from './support-tickets/support-tickets.compo
     ServerStatusComponent,
     TrafficMonitorizationComponent,
     SupportTicketsComponent,
+    DashboardItemComponent,
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
